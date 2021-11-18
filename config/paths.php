0d031f8b4113f77820efe3e362c36ebad5624933
Phrygien <84671612+phrygien@users.noreply.github.com>
2021-11-18 11:39:10 +0300

    Initial commit

---
+++ config/paths.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+	"controllersPath" => "App/Controllers",
+
+	"modelsPath" => "App/Models",
+
+	"migrationsPath" => "App/Database/Migrations",
+
+	"seedsPath" => "App/Database/Seeds",
+
+	"factoriesPath" => "App/Database/Factories",
+
+	"helpersPath" => "App/Helpers",
+
+	"viewsPath" => "App/Views",
+
+	"configPath" => "App/Config",
+
+	"storagePath" => "storage",
+
+	"commandsPath" => "App/Console",
+
+	"routesPath" => "App/Routes",
+
+	"libPath" => "Lib",
+
+	"publicPath" => "public",
+
+    "databaseStoragePath" => "storage/app/db"
+];
